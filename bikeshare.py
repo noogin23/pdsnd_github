9f73b611b292bcb2729b49aa331753c974afc64d
Justin Nguyen <justin.nguyen@effem.com>
2020-04-17 09:48:05 -0500

    Add thank you message to main function

--- bikeshare.py
+++ bikeshare.py
@@ -197,6 +197,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("\nThank You for using this progam")
             break
 
 
